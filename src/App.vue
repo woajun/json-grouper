f4faef2671ea433d068a006cc288841b71bfa840
juni <woajun@naver.com>
2023-03-07 09:37:05 +0900

    add json viewer VueDd

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="container h-screen font-semibold text-3xl text-gray-800 w-screen fixed">
-    <div>
-      <RouterView />
-    </div>
+  <div>
+    <RouterView />
   </div>
 </template>
--- src/pages/CalcPage.vue
+++ src/pages/CalcPage.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { VueDd } from 'vue-dd';
 import apple from '../apple';
 
 function tie(arr:Array<any>, key: string, leftKey: string[], childKey: string): Array<any> {
@@ -43,24 +45,43 @@ function keyChanges(arr: Array<any>, fromTo: [string, string][]) {
   return result;
 }
 
-const bigs = tie(apple, 'bigPath', ['bigName'], 'mids');
-const bigsChanged = keyChanges(bigs, [['bigPath', 'path'], ['bigName', 'name']]);
-const result = bigsChanged.map((big) => {
-  const mids = tie(big.mids, 'midID', ['midName', 'midPath'], 'views');
-  const changedMids = keyChanges(mids, [['midID', 'id'], ['midName', 'name'], ['midPath', 'middlePath']]);
-  const r = changedMids.map((e) => ({
-    ...e,
-    views: keyChanges(e.views, [['smID', 'id'], ['smName', 'name']]),
-  }));
-  return {
-    name: big.name,
-    path: big.path,
-    mids: r,
-  };
-});
-console.log(result);
+const text = ref('');
 
+const resultObj = ref<any>([]);
+function calc() {
+  const bigs = tie(apple, 'bigPath', ['bigName'], 'mids');
+  const bigsChanged = keyChanges(bigs, [['bigPath', 'path'], ['bigName', 'name']]);
+  const result = bigsChanged.map((big) => {
+    const mids = tie(big.mids, 'midID', ['midName', 'midPath'], 'views');
+    const changedMids = keyChanges(mids, [['midID', 'id'], ['midName', 'name'], ['midPath', 'middlePath']]);
+    const r = changedMids.map((e) => ({
+      ...e,
+      views: keyChanges(e.views, [['smID', 'id'], ['smName', 'name']]),
+    }));
+    return {
+      name: big.name,
+      path: big.path,
+      mids: r,
+    };
+  });
+  console.log(result);
+  resultObj.value = result;
+}
 </script>
 <template>
-  <div>ssssss</div>
+  <div>
+    <label>
+      json
+      <input v-model="text" class="border-4" type="text">
+    </label>
+    <button class="rounded-full bg-indigo-500" @click="calc">
+      계산
+    </button>
+    <div>
+      <VueDd v-model="resultObj" font-size="1rem" max-height="800px" />
+    </div>
+    <button class="rounded-full bg-indigo-500" @click="calc">
+      텍스트 복사
+    </button>
+  </div>
 </template>
