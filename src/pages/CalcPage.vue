7c7a7ed3da95afb4f8bf228e1b0e0caa1bad3bf4
juni <woajun@naver.com>
2023-03-07 10:23:44 +0900

    extract makeGroup

--- src/pages/CalcPage.vue
+++ src/pages/CalcPage.vue
@@ -1,62 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { VueDd } from 'vue-dd';
-import apple from '../apple';
+import { makeGroup, changeArrayPropertyNames} from './makeGroup';
+import data from '../apple';
 
-function tie(arr:Array<any>, key: string, leftKey: string[], childKey: string): Array<any> {
-  function findAll(id: string) {
-    const filtered = arr.filter((e) => e[key] === id);
-    filtered.forEach((e) => {
-      delete e[key];
-      leftKey.forEach((k) => {
-        delete e[k];
-      });
-    });
-    return filtered;
-  }
-  const criterias = arr.map((e) => e[key]);
-  const deduplicaition = [...new Set(criterias)];
-  return deduplicaition.map((criteria) => {
-    const found = arr.find((e) => e[key] === criteria);
-    const result = {
-      [key]: found[key],
-    };
-    leftKey.forEach((e) => {
-      result[e] = found[e];
-    });
-    result[childKey] = findAll(criteria);
-    return result;
-  });
-}
-
-function keyChange(arr:Array<any>, from: string, to: string) {
-  return arr.map((e) => {
-    const a = {[to]: e[from], ...e};
-    delete a[from];
-    return a;
-  });
-}
-
-function keyChanges(arr: Array<any>, fromTo: [string, string][]) {
-  let result = arr;
-  for (let i = 0; i < fromTo.length; i += 1) {
-    result = keyChange(result, fromTo[i][0], fromTo[i][1]);
-  }
-  return result;
-}
-
-const text = ref('');
+const text = ref(JSON.stringify(data, null, 2));
 
 const resultObj = ref<any>([]);
 function calc() {
-  const bigs = tie(apple, 'bigPath', ['bigName'], 'mids');
-  const bigsChanged = keyChanges(bigs, [['bigPath', 'path'], ['bigName', 'name']]);
+  const bigs = makeGroup(data, 'bigPath', ['bigName'], 'mids');
+  const bigsChanged = changeArrayPropertyNames(bigs, [['bigPath', 'path'], ['bigName', 'name']]);
   const result = bigsChanged.map((big) => {
-    const mids = tie(big.mids, 'midID', ['midName', 'midPath'], 'views');
-    const changedMids = keyChanges(mids, [['midID', 'id'], ['midName', 'name'], ['midPath', 'middlePath']]);
+    const mids = makeGroup(big.mids, 'midID', ['midName', 'midPath'], 'views');
+    const changedMids = changeArrayPropertyNames(mids, [['midID', 'id'], ['midName', 'name'], ['midPath', 'middlePath']]);
     const r = changedMids.map((e) => ({
       ...e,
-      views: keyChanges(e.views, [['smID', 'id'], ['smName', 'name']]),
+      views: changeArrayPropertyNames(e.views, [['smID', 'id'], ['smName', 'name']]),
     }));
     return {
       name: big.name,
@@ -72,7 +31,7 @@ function calc() {
   <div>
     <label>
       json
-      <input v-model="text" class="border-4" type="text">
+      <textarea v-model="text" class="border-2" cols="100" rows="10" />
     </label>
     <button class="rounded-full bg-indigo-500" @click="calc">
       계산
---
+++ src/pages/makeGroup.ts
@@ -0,0 +1,41 @@
+export function makeGroup(arr:Array<any>, key: string, leftKey: string[], childKey: string): Array<any> {
+  function findAll(id: string) {
+    const filtered = arr.filter((e) => e[key] === id);
+    filtered.forEach((e) => {
+      delete e[key];
+      leftKey.forEach((k) => {
+        delete e[k];
+      });
+    });
+    return filtered;
+  }
+  const criterias = arr.map((e) => e[key]);
+  const deduplicaition = [...new Set(criterias)];
+  return deduplicaition.map((criteria) => {
+    const found = arr.find((e) => e[key] === criteria);
+    const result = {
+      [key]: found[key],
+    };
+    leftKey.forEach((e) => {
+      result[e] = found[e];
+    });
+    result[childKey] = findAll(criteria);
+    return result;
+  });
+}
+
+export function changeArrayPropertyName(arr:Array<any>, from: string, to: string) {
+  return arr.map((e) => {
+    const a = {[to]: e[from], ...e};
+    delete a[from];
+    return a;
+  });
+}
+
+export function changeArrayPropertyNames(arr: Array<any>, fromToArray: [string, string][]) {
+  let result = arr;
+  for (let i = 0; i < fromToArray.length; i += 1) {
+    result = changeArrayPropertyName(result, fromToArray[i][0], fromToArray[i][1]);
+  }
+  return result;
+}
